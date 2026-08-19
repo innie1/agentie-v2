eba3f47d829ab9d6f5ec68742806875c95dcbc09
innie1 <101974666+innie1@users.noreply.github.com>
2026-08-19 14:01:56 +0100

    Add duplicate cleanup tools to permissioned toolsets

--- agentie/tools/registry.py
+++ agentie/tools/registry.py
@@ -5,16 +5,33 @@ from agentie.tools.github_tools import github_read_file, github_repo_info
 from agentie.tools.http_tools import http_get
 from agentie.tools.memory_tools import list_memories, recall_memory, remember
 from agentie.tools.python_tools import run_python
-from agentie.tools.task_tools import create_task, list_tasks, update_task
+from agentie.tools.task_tools import (
+    create_task,
+    delete_task,
+    find_duplicate_tasks,
+    list_tasks,
+    request_task_delete_approval,
+    update_task,
+)
 from agentie.tools.web_tools import search_web
 
+TASK_TOOLS = [
+    create_task,
+    list_tasks,
+    update_task,
+    find_duplicate_tasks,
+    request_task_delete_approval,
+    delete_task,
+]
+
 TOOLSETS = {
-    "general": [get_current_utc_time, search_web, read_text_file, write_text_file, list_workspace_files, edit_text_file, run_python, remember, recall_memory, list_memories, create_task, list_tasks, update_task, http_get, request_approval, list_approvals],
-    "research": [get_current_utc_time, search_web, http_get, read_text_file, write_text_file, list_workspace_files, remember, recall_memory, list_memories, create_task, list_tasks, update_task],
-    "coding": [get_current_utc_time, read_text_file, write_text_file, list_workspace_files, edit_text_file, run_python, github_repo_info, github_read_file, create_task, list_tasks, update_task, request_approval, list_approvals],
-    "manager": [get_current_utc_time, remember, recall_memory, list_memories, create_task, list_tasks, update_task, request_approval, list_approvals],
-    "github": [github_repo_info, github_read_file, read_text_file, write_text_file, list_workspace_files, create_task, list_tasks, update_task, request_approval, list_approvals],
+    "general": [get_current_utc_time, search_web, read_text_file, write_text_file, list_workspace_files, edit_text_file, run_python, remember, recall_memory, list_memories, *TASK_TOOLS, http_get, request_approval, list_approvals],
+    "research": [get_current_utc_time, search_web, http_get, read_text_file, write_text_file, list_workspace_files, remember, recall_memory, list_memories, *TASK_TOOLS],
+    "coding": [get_current_utc_time, read_text_file, write_text_file, list_workspace_files, edit_text_file, run_python, github_repo_info, github_read_file, *TASK_TOOLS, request_approval, list_approvals],
+    "manager": [get_current_utc_time, remember, recall_memory, list_memories, *TASK_TOOLS, request_approval, list_approvals],
+    "github": [github_repo_info, github_read_file, read_text_file, write_text_file, list_workspace_files, *TASK_TOOLS, request_approval, list_approvals],
 }
+
 
 def tools_for(agent_type: str):
     return TOOLSETS.get(agent_type, TOOLSETS["general"])
