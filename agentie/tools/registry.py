bdb483ad93fe2b1d16fe5bffce48866af95372ae
innie1 <101974666+innie1@users.noreply.github.com>
2026-08-19 14:40:01 +0100

    Register advanced local utilities across permissioned agent profiles

--- agentie/tools/registry.py
+++ agentie/tools/registry.py
@@ -1,3 +1,24 @@
+from agentie.tools.advanced_utility_tools import (
+    cancel_recurring_schedule,
+    compare_json_text,
+    countdown_to,
+    create_recurring_schedule,
+    date_difference,
+    detailed_system_status,
+    file_checksum,
+    format_json_text,
+    format_yaml_text,
+    image_metadata,
+    list_recurring_schedules,
+    local_datetime,
+    rss_read,
+    scratchpad_get,
+    scratchpad_list,
+    scratchpad_set,
+    unzip_workspace_archive,
+    wikipedia_lookup,
+    zip_workspace_files,
+)
 from agentie.tools.approval_tools import list_approvals, request_approval
 from agentie.tools.basic_tools import get_current_utc_time
 from agentie.tools.browser_tools import browser_read_page
@@ -40,45 +61,37 @@ from agentie.tools.task_tools import (
 )
 from agentie.tools.web_tools import search_web
 
-TASK_TOOLS = [
-    create_task,
-    list_tasks,
-    update_task,
-    find_duplicate_tasks,
-    request_task_delete_approval,
-    delete_task,
-]
-
+TASK_TOOLS = [create_task, list_tasks, update_task, find_duplicate_tasks, request_task_delete_approval, delete_task]
 DOCUMENT_TOOLS = [read_pdf, read_csv]
-LOCAL_UTILITY_TOOLS = [
-    set_timer,
-    set_alarm_at,
-    list_timers,
-    cancel_timer,
-    stopwatch_start,
-    stopwatch_pause,
-    stopwatch_reset,
-    stopwatch_status,
-    weather_lookup,
+LOCAL_UTILITY_TOOLS = [set_timer, set_alarm_at, list_timers, cancel_timer, stopwatch_start, stopwatch_pause, stopwatch_reset, stopwatch_status, weather_lookup]
+PRODUCTIVITY_TOOLS = [calculate, convert_unit, create_reminder, list_reminders, cancel_reminder, save_note, list_notes, read_note, system_status]
+ADVANCED_LOCAL_TOOLS = [
+    local_datetime,
+    date_difference,
+    countdown_to,
+    create_recurring_schedule,
+    list_recurring_schedules,
+    cancel_recurring_schedule,
+    scratchpad_set,
+    scratchpad_get,
+    scratchpad_list,
+    zip_workspace_files,
+    unzip_workspace_archive,
+    format_json_text,
+    format_yaml_text,
+    compare_json_text,
+    file_checksum,
+    image_metadata,
+    detailed_system_status,
 ]
-PRODUCTIVITY_TOOLS = [
-    calculate,
-    convert_unit,
-    create_reminder,
-    list_reminders,
-    cancel_reminder,
-    save_note,
-    list_notes,
-    read_note,
-    system_status,
-]
+FREE_KNOWLEDGE_TOOLS = [rss_read, wikipedia_lookup]
 
 TOOLSETS = {
-    "general": [get_current_utc_time, *LOCAL_UTILITY_TOOLS, *PRODUCTIVITY_TOOLS, search_web, browser_read_page, http_get, read_text_file, write_text_file, list_workspace_files, edit_text_file, *DOCUMENT_TOOLS, run_python, remember, recall_memory, list_memories, *TASK_TOOLS, supabase_select, supabase_insert, request_approval, list_approvals],
-    "research": [get_current_utc_time, weather_lookup, calculate, convert_unit, save_note, list_notes, read_note, search_web, browser_read_page, http_get, read_text_file, write_text_file, list_workspace_files, *DOCUMENT_TOOLS, remember, recall_memory, list_memories, *TASK_TOOLS],
-    "coding": [get_current_utc_time, set_timer, list_timers, stopwatch_start, stopwatch_pause, stopwatch_reset, stopwatch_status, calculate, convert_unit, system_status, read_text_file, write_text_file, list_workspace_files, edit_text_file, *DOCUMENT_TOOLS, run_python, github_repo_info, github_read_file, *TASK_TOOLS, request_approval, list_approvals],
-    "manager": [get_current_utc_time, *LOCAL_UTILITY_TOOLS, *PRODUCTIVITY_TOOLS, remember, recall_memory, list_memories, *TASK_TOOLS, supabase_select, supabase_insert, request_approval, list_approvals],
-    "github": [github_repo_info, github_read_file, read_text_file, write_text_file, list_workspace_files, system_status, *TASK_TOOLS, request_approval, list_approvals],
+    "general": [get_current_utc_time, *LOCAL_UTILITY_TOOLS, *PRODUCTIVITY_TOOLS, *ADVANCED_LOCAL_TOOLS, *FREE_KNOWLEDGE_TOOLS, search_web, browser_read_page, http_get, read_text_file, write_text_file, list_workspace_files, edit_text_file, *DOCUMENT_TOOLS, run_python, remember, recall_memory, list_memories, *TASK_TOOLS, supabase_select, supabase_insert, request_approval, list_approvals],
+    "research": [get_current_utc_time, local_datetime, weather_lookup, calculate, convert_unit, save_note, list_notes, read_note, scratchpad_set, scratchpad_get, scratchpad_list, rss_read, wikipedia_lookup, search_web, browser_read_page, http_get, read_text_file, write_text_file, list_workspace_files, *DOCUMENT_TOOLS, remember, recall_memory, list_memories, *TASK_TOOLS],
+    "coding": [get_current_utc_time, local_datetime, set_timer, list_timers, stopwatch_start, stopwatch_pause, stopwatch_reset, stopwatch_status, calculate, convert_unit, detailed_system_status, scratchpad_set, scratchpad_get, scratchpad_list, zip_workspace_files, unzip_workspace_archive, format_json_text, format_yaml_text, compare_json_text, file_checksum, image_metadata, read_text_file, write_text_file, list_workspace_files, edit_text_file, *DOCUMENT_TOOLS, run_python, github_repo_info, github_read_file, *TASK_TOOLS, request_approval, list_approvals],
+    "manager": [get_current_utc_time, *LOCAL_UTILITY_TOOLS, *PRODUCTIVITY_TOOLS, local_datetime, date_difference, countdown_to, create_recurring_schedule, list_recurring_schedules, cancel_recurring_schedule, scratchpad_set, scratchpad_get, scratchpad_list, detailed_system_status, remember, recall_memory, list_memories, *TASK_TOOLS, supabase_select, supabase_insert, request_approval, list_approvals],
+    "github": [github_repo_info, github_read_file, read_text_file, write_text_file, list_workspace_files, detailed_system_status, file_checksum, zip_workspace_files, unzip_workspace_archive, *TASK_TOOLS, request_approval, list_approvals],
 }
 
 
