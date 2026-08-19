5223712bc63f9a71478cc4c6aee1a987dc27e070
innie1 <101974666+innie1@users.noreply.github.com>
2026-08-19 14:14:16 +0100

    Add browser document and Supabase tools to permissioned profiles

--- agentie/tools/registry.py
+++ agentie/tools/registry.py
@@ -1,10 +1,13 @@
 from agentie.tools.approval_tools import list_approvals, request_approval
 from agentie.tools.basic_tools import get_current_utc_time
+from agentie.tools.browser_tools import browser_read_page
+from agentie.tools.document_tools import read_csv, read_pdf
 from agentie.tools.file_tools import edit_text_file, list_workspace_files, read_text_file, write_text_file
 from agentie.tools.github_tools import github_read_file, github_repo_info
 from agentie.tools.http_tools import http_get
 from agentie.tools.memory_tools import list_memories, recall_memory, remember
 from agentie.tools.python_tools import run_python
+from agentie.tools.supabase_tools import supabase_insert, supabase_select
 from agentie.tools.task_tools import (
     create_task,
     delete_task,
@@ -24,11 +27,13 @@ TASK_TOOLS = [
     delete_task,
 ]
 
+DOCUMENT_TOOLS = [read_pdf, read_csv]
+
 TOOLSETS = {
-    "general": [get_current_utc_time, search_web, read_text_file, write_text_file, list_workspace_files, edit_text_file, run_python, remember, recall_memory, list_memories, *TASK_TOOLS, http_get, request_approval, list_approvals],
-    "research": [get_current_utc_time, search_web, http_get, read_text_file, write_text_file, list_workspace_files, remember, recall_memory, list_memories, *TASK_TOOLS],
-    "coding": [get_current_utc_time, read_text_file, write_text_file, list_workspace_files, edit_text_file, run_python, github_repo_info, github_read_file, *TASK_TOOLS, request_approval, list_approvals],
-    "manager": [get_current_utc_time, remember, recall_memory, list_memories, *TASK_TOOLS, request_approval, list_approvals],
+    "general": [get_current_utc_time, search_web, browser_read_page, http_get, read_text_file, write_text_file, list_workspace_files, edit_text_file, *DOCUMENT_TOOLS, run_python, remember, recall_memory, list_memories, *TASK_TOOLS, supabase_select, supabase_insert, request_approval, list_approvals],
+    "research": [get_current_utc_time, search_web, browser_read_page, http_get, read_text_file, write_text_file, list_workspace_files, *DOCUMENT_TOOLS, remember, recall_memory, list_memories, *TASK_TOOLS],
+    "coding": [get_current_utc_time, read_text_file, write_text_file, list_workspace_files, edit_text_file, *DOCUMENT_TOOLS, run_python, github_repo_info, github_read_file, *TASK_TOOLS, request_approval, list_approvals],
+    "manager": [get_current_utc_time, remember, recall_memory, list_memories, *TASK_TOOLS, supabase_select, supabase_insert, request_approval, list_approvals],
     "github": [github_repo_info, github_read_file, read_text_file, write_text_file, list_workspace_files, *TASK_TOOLS, request_approval, list_approvals],
 }
 
