f9ec3c97ce78633cb75642c17e17e4ec11f5570c
innie1 <101974666+innie1@users.noreply.github.com>
2026-08-19 18:18:52 +0100

    Expose semantic memory search to agents

--- agentie/tools/registry.py
+++ agentie/tools/registry.py
@@ -18,7 +18,7 @@ from agentie.tools.local_utility_tools import (
     cancel_timer, list_timers, set_alarm_at, set_timer, stopwatch_pause, stopwatch_reset,
     stopwatch_start, stopwatch_status, weather_lookup,
 )
-from agentie.tools.memory_tools import list_memories, recall_memory, remember
+from agentie.tools.memory_tools import list_memories, recall_memory, remember, search_memory
 from agentie.tools.productivity_tools import (
     calculate, cancel_reminder, convert_unit, create_reminder, list_notes, list_reminders,
     read_note, save_note, system_status,
@@ -34,6 +34,7 @@ TASK_TOOLS=[create_task,list_tasks,update_task,find_duplicate_tasks,request_task
 DOCUMENT_TOOLS=[read_pdf,read_csv]
 ARTIFACT_TOOLS=[create_word_document,create_excel_workbook,create_powerpoint_presentation]
 COLLECTION_TOOLS=[collection_create,collection_list,collection_index_file,collection_search,collection_context]
+MEMORY_TOOLS=[remember,recall_memory,search_memory,list_memories]
 LOCAL_UTILITY_TOOLS=[set_timer,set_alarm_at,list_timers,cancel_timer,stopwatch_start,stopwatch_pause,stopwatch_reset,stopwatch_status,weather_lookup]
 PRODUCTIVITY_TOOLS=[calculate,convert_unit,create_reminder,list_reminders,cancel_reminder,save_note,list_notes,read_note,system_status]
 ADVANCED_LOCAL_TOOLS=[local_datetime,date_difference,countdown_to,create_recurring_schedule,list_recurring_schedules,cancel_recurring_schedule,scratchpad_set,scratchpad_get,scratchpad_list,zip_workspace_files,unzip_workspace_archive,format_json_text,format_yaml_text,compare_json_text,file_checksum,image_metadata,detailed_system_status]
@@ -42,11 +43,11 @@ FILE_TOOLS=[read_text_file,write_text_file,list_workspace_files,edit_text_file,*
 RESEARCH_TOOLS=[search_web,browser_read_page,http_get,*FREE_KNOWLEDGE_TOOLS]
 
 BASE_TOOLSETS={
- "general":[get_current_utc_time,*LOCAL_UTILITY_TOOLS,*PRODUCTIVITY_TOOLS,*ADVANCED_LOCAL_TOOLS,*RESEARCH_TOOLS,*FILE_TOOLS,run_python,remember,recall_memory,list_memories,*TASK_TOOLS,supabase_select,supabase_insert,request_approval,list_approvals],
- "research":[get_current_utc_time,local_datetime,weather_lookup,calculate,convert_unit,save_note,list_notes,read_note,scratchpad_set,scratchpad_get,scratchpad_list,*RESEARCH_TOOLS,*FILE_TOOLS,remember,recall_memory,list_memories,*TASK_TOOLS],
- "coding":[get_current_utc_time,local_datetime,set_timer,list_timers,stopwatch_start,stopwatch_pause,stopwatch_reset,stopwatch_status,calculate,convert_unit,detailed_system_status,scratchpad_set,scratchpad_get,scratchpad_list,zip_workspace_files,unzip_workspace_archive,format_json_text,format_yaml_text,compare_json_text,file_checksum,image_metadata,*FILE_TOOLS,run_python,github_repo_info,github_read_file,*TASK_TOOLS,request_approval,list_approvals],
- "manager":[get_current_utc_time,*LOCAL_UTILITY_TOOLS,*PRODUCTIVITY_TOOLS,local_datetime,date_difference,countdown_to,create_recurring_schedule,list_recurring_schedules,cancel_recurring_schedule,scratchpad_set,scratchpad_get,scratchpad_list,detailed_system_status,*COLLECTION_TOOLS,*ARTIFACT_TOOLS,remember,recall_memory,list_memories,*TASK_TOOLS,supabase_select,supabase_insert,request_approval,list_approvals],
- "github":[github_repo_info,github_read_file,read_text_file,write_text_file,list_workspace_files,detailed_system_status,file_checksum,zip_workspace_files,unzip_workspace_archive,*TASK_TOOLS,request_approval,list_approvals],
+ "general":[get_current_utc_time,*LOCAL_UTILITY_TOOLS,*PRODUCTIVITY_TOOLS,*ADVANCED_LOCAL_TOOLS,*RESEARCH_TOOLS,*FILE_TOOLS,run_python,*MEMORY_TOOLS,*TASK_TOOLS,supabase_select,supabase_insert,request_approval,list_approvals],
+ "research":[get_current_utc_time,local_datetime,weather_lookup,calculate,convert_unit,save_note,list_notes,read_note,scratchpad_set,scratchpad_get,scratchpad_list,*RESEARCH_TOOLS,*FILE_TOOLS,*MEMORY_TOOLS,*TASK_TOOLS],
+ "coding":[get_current_utc_time,local_datetime,set_timer,list_timers,stopwatch_start,stopwatch_pause,stopwatch_reset,stopwatch_status,calculate,convert_unit,detailed_system_status,scratchpad_set,scratchpad_get,scratchpad_list,zip_workspace_files,unzip_workspace_archive,format_json_text,format_yaml_text,compare_json_text,file_checksum,image_metadata,*FILE_TOOLS,run_python,github_repo_info,github_read_file,*MEMORY_TOOLS,*TASK_TOOLS,request_approval,list_approvals],
+ "manager":[get_current_utc_time,*LOCAL_UTILITY_TOOLS,*PRODUCTIVITY_TOOLS,local_datetime,date_difference,countdown_to,create_recurring_schedule,list_recurring_schedules,cancel_recurring_schedule,scratchpad_set,scratchpad_get,scratchpad_list,detailed_system_status,*COLLECTION_TOOLS,*ARTIFACT_TOOLS,*MEMORY_TOOLS,*TASK_TOOLS,supabase_select,supabase_insert,request_approval,list_approvals],
+ "github":[github_repo_info,github_read_file,read_text_file,write_text_file,list_workspace_files,detailed_system_status,file_checksum,zip_workspace_files,unzip_workspace_archive,*MEMORY_TOOLS,*TASK_TOOLS,request_approval,list_approvals],
 }
 
 
