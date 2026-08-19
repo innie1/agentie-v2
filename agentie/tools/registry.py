8815c9f5c966fcc91748e72f30949f88315132ef
innie1 <101974666+innie1@users.noreply.github.com>
2026-08-19 18:05:47 +0100

    Register local Office artifact tools

--- agentie/tools/registry.py
+++ agentie/tools/registry.py
@@ -6,6 +6,7 @@ from agentie.tools.advanced_utility_tools import (
     scratchpad_list, scratchpad_set, unzip_workspace_archive, wikipedia_lookup, zip_workspace_files,
 )
 from agentie.tools.approval_tools import list_approvals, request_approval
+from agentie.tools.artifact_tools import create_excel_workbook, create_powerpoint_presentation, create_word_document
 from agentie.tools.basic_tools import get_current_utc_time
 from agentie.tools.browser_tools import browser_read_page
 from agentie.tools.collection_tools import collection_context, collection_create, collection_index_file, collection_list, collection_search
@@ -31,19 +32,20 @@ from agentie.tools.web_tools import search_web
 
 TASK_TOOLS=[create_task,list_tasks,update_task,find_duplicate_tasks,request_task_delete_approval,delete_task]
 DOCUMENT_TOOLS=[read_pdf,read_csv]
+ARTIFACT_TOOLS=[create_word_document,create_excel_workbook,create_powerpoint_presentation]
 COLLECTION_TOOLS=[collection_create,collection_list,collection_index_file,collection_search,collection_context]
 LOCAL_UTILITY_TOOLS=[set_timer,set_alarm_at,list_timers,cancel_timer,stopwatch_start,stopwatch_pause,stopwatch_reset,stopwatch_status,weather_lookup]
 PRODUCTIVITY_TOOLS=[calculate,convert_unit,create_reminder,list_reminders,cancel_reminder,save_note,list_notes,read_note,system_status]
 ADVANCED_LOCAL_TOOLS=[local_datetime,date_difference,countdown_to,create_recurring_schedule,list_recurring_schedules,cancel_recurring_schedule,scratchpad_set,scratchpad_get,scratchpad_list,zip_workspace_files,unzip_workspace_archive,format_json_text,format_yaml_text,compare_json_text,file_checksum,image_metadata,detailed_system_status]
 FREE_KNOWLEDGE_TOOLS=[rss_read,wikipedia_lookup]
-FILE_TOOLS=[read_text_file,write_text_file,list_workspace_files,edit_text_file,*DOCUMENT_TOOLS,*COLLECTION_TOOLS]
+FILE_TOOLS=[read_text_file,write_text_file,list_workspace_files,edit_text_file,*DOCUMENT_TOOLS,*COLLECTION_TOOLS,*ARTIFACT_TOOLS]
 RESEARCH_TOOLS=[search_web,browser_read_page,http_get,*FREE_KNOWLEDGE_TOOLS]
 
 BASE_TOOLSETS={
  "general":[get_current_utc_time,*LOCAL_UTILITY_TOOLS,*PRODUCTIVITY_TOOLS,*ADVANCED_LOCAL_TOOLS,*RESEARCH_TOOLS,*FILE_TOOLS,run_python,remember,recall_memory,list_memories,*TASK_TOOLS,supabase_select,supabase_insert,request_approval,list_approvals],
  "research":[get_current_utc_time,local_datetime,weather_lookup,calculate,convert_unit,save_note,list_notes,read_note,scratchpad_set,scratchpad_get,scratchpad_list,*RESEARCH_TOOLS,*FILE_TOOLS,remember,recall_memory,list_memories,*TASK_TOOLS],
  "coding":[get_current_utc_time,local_datetime,set_timer,list_timers,stopwatch_start,stopwatch_pause,stopwatch_reset,stopwatch_status,calculate,convert_unit,detailed_system_status,scratchpad_set,scratchpad_get,scratchpad_list,zip_workspace_files,unzip_workspace_archive,format_json_text,format_yaml_text,compare_json_text,file_checksum,image_metadata,*FILE_TOOLS,run_python,github_repo_info,github_read_file,*TASK_TOOLS,request_approval,list_approvals],
- "manager":[get_current_utc_time,*LOCAL_UTILITY_TOOLS,*PRODUCTIVITY_TOOLS,local_datetime,date_difference,countdown_to,create_recurring_schedule,list_recurring_schedules,cancel_recurring_schedule,scratchpad_set,scratchpad_get,scratchpad_list,detailed_system_status,*COLLECTION_TOOLS,remember,recall_memory,list_memories,*TASK_TOOLS,supabase_select,supabase_insert,request_approval,list_approvals],
+ "manager":[get_current_utc_time,*LOCAL_UTILITY_TOOLS,*PRODUCTIVITY_TOOLS,local_datetime,date_difference,countdown_to,create_recurring_schedule,list_recurring_schedules,cancel_recurring_schedule,scratchpad_set,scratchpad_get,scratchpad_list,detailed_system_status,*COLLECTION_TOOLS,*ARTIFACT_TOOLS,remember,recall_memory,list_memories,*TASK_TOOLS,supabase_select,supabase_insert,request_approval,list_approvals],
  "github":[github_repo_info,github_read_file,read_text_file,write_text_file,list_workspace_files,detailed_system_status,file_checksum,zip_workspace_files,unzip_workspace_archive,*TASK_TOOLS,request_approval,list_approvals],
 }
 
