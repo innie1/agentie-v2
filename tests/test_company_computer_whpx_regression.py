62851dd0fe860e3b76ac9b8210b1ba13f481e02b
innie1 <101974666+innie1@users.noreply.github.com>
2026-08-23 21:50:41 +0100

    Cover WHPX single-vCPU stability rule

--- tests/test_company_computer_whpx_regression.py
+++ tests/test_company_computer_whpx_regression.py
@@ -27,9 +27,16 @@ class CompanyComputerWHPXRegressionTests(unittest.TestCase):
         self.assertNotIn("-cpu host", joined)
         self.assertIn("-machine q35", joined)
 
-    def test_non_whpx_x86_keeps_cpu_host(self):
+    def test_windows_whpx_forces_single_vcpu_for_apic_stability(self):
+        args = cc._qemu_args(self._config())
+        smp_index = args.index("-smp")
+        self.assertEqual(args[smp_index + 1], "1")
+
+    def test_non_whpx_x86_keeps_cpu_host_and_configured_vcpus(self):
         args = cc._qemu_args(self._config(accelerator="kvm"))
         self.assertIn("-cpu host", " ".join(args))
+        smp_index = args.index("-smp")
+        self.assertEqual(args[smp_index + 1], "2")
 
     def test_whpx_fix_keeps_persistent_disk_and_guest_agent_channel(self):
         args = cc._qemu_args(self._config())
